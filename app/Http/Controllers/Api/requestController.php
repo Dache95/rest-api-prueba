c91542fbe18fc2b95293fd7a5b4792494b64c726
Dache95 <jparedes123469@gmail.com>
2024-09-09 02:10:52 -0600

    REST API prueba

---
+++ app/Http/Controllers/Api/requestController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Requests;
+use Illuminate\Support\Facades\Validator;
+
+class requestController extends Controller
+{
+    public function index() {
+        $requests = Requests::all();
+
+        return $requests->isNotEmpty()
+            ? response()->json($requests, 200)
+            : response()->json(['message' => 'No se encontraron solicitudes'], 404);
+    }
+
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'titulo' => 'required',
+            'descripcion' => 'required',
+            'estado' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Error al validar los datos',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ], 400);
+        }
+
+        $requestData = $request->only(['titulo', 'descripcion', 'estado']);
+        $newRequest = Requests::create($requestData);
+
+        if (!$newRequest) {
+            return response()->json(['message' => 'Error al crear la solicitud'], 500);
+        }
+
+        return response()->json([
+            'request' => $newRequest,
+            'status' => 201
+        ], 201);
+    }
+
+
+}
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\userController;
+use App\Http\Controllers\Api\requestController;
+use App\Http\Controllers\Api\assignmentsController;
+
+
+
+Route::get('/user', [userController::class, 'index']);
+
+Route::get('/requests', [requestController::class, 'index']);
+Route::post('/requests', [requestController::class, 'store']);
+
+Route::get('/assignments', [assignmentsController::class, 'index']);
+Route::post('/requests/{id}/assign', [AssignmentsController::class, 'assign']);
